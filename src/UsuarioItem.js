bf9a8f919a7dc1c5e4d2751a143392e4f4e1a2e6
Marcio Sansone <msansone@gmail.com>
2017-11-29 15:29:35 -0200

    implementando atualização

--- src/UsuarioItem.js
+++ src/UsuarioItem.js
@@ -4,10 +4,13 @@ class UsuarioItem extends React.Component
     constructor(props){
         super(props)
         this.carregaDetalhe=this.carregaDetalhe.bind(this)
-        this.state={items:[]}
+        this.salvar=this.salvar.bind(this)
+        this.changeNome=this.changeNome.bind(this)
+        this.state={items:{"id":"0","email":"email","senha":"senha","nome":"nome"}}
     }
 
     carregaDetalhe(id){
+        console.log('carregaDetalhe(id)')
         let url='http://msansone.com.br:8080/usuarios/'+id
         console.log(url)
         fetch(url)
@@ -35,16 +38,48 @@ class UsuarioItem extends React.Component
         var id = newProps.match.params.id
         this.carregaDetalhe(id)         
     }
+    salvar(){
+        console.log('salvar()')
+        fetch('http://msansone.com.br:8080/usuarios/', {
+            method: 'PUT',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+            },
+        body: JSON.stringify(this.state.items)  
+        })
+    }
+
+    changeNome(e){
+        //alert(this.state.items[0])
+        //this.state.items.nome=e.target.value
+        
+        //var obj = JSON.parse(this.state.items[0])
+        //obj.nome=e.target.value
+        //this.setState(JSON.stringify(obj))
+    }
 
     render(){
         console.log('render()')
         return(
             <div className='row'>
+            <div>{JSON.stringify(this.state.items[0])}</div>
             
             <div>Usuario:</div><br/>
-            <div className= 'col-md-2'>Nome:</div> <div className= 'col-md-10'><input type = "text" value = {this.state.items.nome}/></div>
-            <div className= 'col-md-2'>E-mail:</div> <div className= 'col-md-10'><input type = "text" value = {this.state.items.email}/></div>
-            
+            <div className= 'col-md-2'>Nome:</div> 
+            <div className= 'col-md-10'><input  type = "text" 
+                                                value = {this.state.items.nome} 
+                                                onChange={() => {}} 
+                                                />
+            </div>
+            <div className= 'col-md-2'>E-mail:</div> 
+            <div className= 'col-md-10'><input  type = "text" 
+                                                />
+            </div><br/>
+            <div className= 'col-md-12'><input  type = "button" 
+                                                value="SALVAR"  
+                                                onClick={this.salvar}/> 
+            </div> 
         </div>
         )
     } 
