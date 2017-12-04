73ea2d77a3abd6dd16d1001773ad2e641d3b3f8c
Marcio Sansone <msansone@gmail.com>
2017-12-04 15:53:40 -0200

    Primeira versão com inser de usuario (instavel)

--- src/UsuarioItem.js
+++ src/UsuarioItem.js
@@ -4,8 +4,9 @@ class UsuarioItem extends React.Component {
         super(props)
         this.carregaDetalhe = this.carregaDetalhe.bind(this)
         this.salvar = this.salvar.bind(this)
-        this.changeNome = this.changeNome.bind(this)
-        this.state = { items: { "id": "0", "email": "email", "senha": "senha", "nome": "nome" } }
+        this.limpar = this.limpar.bind(this)
+    
+        this.state = { items: { "id": "", "email": "", "senha": "", "nome": "" } }
     }
 
     carregaDetalhe(id) {
@@ -38,8 +39,58 @@ class UsuarioItem extends React.Component {
         var id = newProps.match.params.id
         this.carregaDetalhe(id)
     }
+
+    limpar() {
+        console.log('limpar()')
+        this.refs.nome.value=""
+        this.refs.email.value=""
+        this.setState({ items: { "id": "", "email": "", "senha": "", "nome": "" } })
+    }
+
     salvar() {
         console.log('salvar()')
+        console.log(this.state.items.id)
+        // INSERIR: Entidade nova
+        if (this.state.items.id==="")
+        {
+            console.log("Inserir")
+            let obj = this.state.items
+            obj.nome=this.refs.nome.value
+            obj.email=this.refs.email.value
+            obj.senha="123456"
+            let jobj = JSON.stringify(obj)
+            fetch('http://msansone.com.br:8080/usuarios', 
+                {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                },
+                body: jobj
+            })
+        }
+        // ALTERAR: Entidade existente
+        else {
+            console.log("Alterar")
+            let obj = this.state.items
+            obj.nome=this.refs.nome.value
+            obj.email=this.refs.email.value
+            let jobj = JSON.stringify(obj)
+            let id = this.props.match.params.id
+            fetch('http://msansone.com.br:8080/usuarios/' + id, {
+                method: 'PUT',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: jobj
+            })
+        }
+    }
+
+    salvar_bkp() {
+        console.log('salvar()')
+        console.log(this.state.items.id)
         let obj = this.state.items
         obj.nome=this.refs.nome.value
         obj.email=this.refs.email.value
@@ -55,29 +106,43 @@ class UsuarioItem extends React.Component {
         })
     }
 
-    changeNome(e) {
-        //alert(this.state.items[0])
-        //this.state.items.nome=e.target.value
-
-        //var obj = JSON.parse(this.state.items[0])
-        //obj.nome=e.target.value
-        //this.setState(JSON.stringify(obj))
-    }
 
     render() {
         console.log('render()')
         return (
-            <div className='form-group'>
-
-                <h2>USER</h2>
-                <label for="nome">Name</label>
-                <input type="text" className="form-control" id="nome" aria-describedby="Name" placeholder="enter name" ref="nome"/>
-                
-                <label for="nome">E-mail</label>
-                <input type="text" className="form-control" id="email" aria-describedby="Email" placeholder="enter email" ref="email"/>
-                <br />
-                <button type="submit" className="btn btn-primary" onClick={this.salvar} >Submit</button>
-            </div>
+            <table className='form-group'>
+            <tbody>
+                <tr>
+                    <td colSpan="2">
+                        <h2><b>USER</b></h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>Name   </label>
+                    </td>
+                    <td>
+                        <input type="text" className="form-control" id="nome" aria-describedby="Name" placeholder="enter name" ref="nome"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label>E-mail   </label>
+                    </td>
+                    <td>
+                        <input type="text" className="form-control" id="email" aria-describedby="Email" placeholder="enter email" ref="email"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                    <br/>
+                        <button type="button" className="btn btn-primary" onClick={this.limpar} >New</button>  
+                        <button type="button" className="btn btn-primary" onClick={this.salvar} >Save</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
         )
     }
 }
